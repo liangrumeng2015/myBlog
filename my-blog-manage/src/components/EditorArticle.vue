62d04a9f21a9984828388d99f69652eff06229ab
liangrumeng <liangrumeng2015@163.com>
2020-05-09 16:47:35 +0800

    管理后台接调试

--- my-blog-manage/src/components/EditorArticle.vue
+++ my-blog-manage/src/components/EditorArticle.vue
@@ -1,48 +1,74 @@
 <template>
   <div>
     <el-form ref="form" :model="form" label-width="80px">
-        <el-input v-model="form.name" placeholder="请输入文章标题"></el-input>
-        
-        <v-VueEditor @editorContent="editorContent" />
-        <br/>
-        <el-button type="primary" @click="releaseArticle">发布文章</el-button>
+      <el-input v-model="form.title" placeholder="请输入文章标题"></el-input>
+
+      <v-VueEditor @editorContent="editorContent" />
+      <br />
+      <el-button type="primary" @click="releaseArticleFn(1)" :loading=releaseLoading>发布文章</el-button>
+      <el-button type="primary" @click="releaseArticleFn(2)">保存草稿</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
-import VueEditor from '@/components/VueEditor'
+import VueEditor from "@/components/VueEditor";
+import { releaseArticle } from "@/config/api";
 export default {
   data() {
     return {
       msg: "124",
-      form:{
-          name:'',
-          content:''
-      }
+      form: {
+        title: "",
+        content: ""
+      },
+      releaseLoading:false
     };
   },
-  components:{
-      'v-VueEditor':VueEditor
+  components: {
+    "v-VueEditor": VueEditor
   },
-  mounted(){
-
-  },
-  methods:{
-      // 富文本编辑器的值
-      editorContent(data){
-          this.form.content = data;
-      },
-      // 发布文章
-      releaseArticle(){
-          console.log('发布文章信息',this.form)
+  mounted() {},
+  methods: {
+    // 富文本编辑器的值
+    editorContent(data) {
+      this.form.content = data;
+    },
+    // 发布/草稿 文章
+    async releaseArticleFn(type) {
+      this.releaseLoading = true
+      const { title, content } = this.form;
+      let data = {
+        articleTitle: title,
+        articleContent: content,
+        articleFirstTime: new Date().getTime(),
+        articleEndTime: new Date().getTime(),
+        articleType: type  // 1发布  2 草稿
+      };
+      const result = await releaseArticle(data);
+      if(result.success){
+        this.releaseLoading = false
+        this.$message({
+          message: result.msg,
+          type: 'success'
+        });
+        setTimeout(()=>{
+          this.$router.push('/articlelist')
+        },1000)
+      }else{
+        this.releaseLoading = false
+        this.$message({
+          message: result.msg,
+          type: 'error'
+        });
       }
+    }
   }
 };
 </script>
 
 <style>
-.el-input__inner{
-    background: #efefef!important;
+.el-input__inner {
+  background: #efefef !important;
 }
 </style>
--- my-blog-manage/src/config/api.js
+++ my-blog-manage/src/config/api.js
@@ -3,15 +3,12 @@ import Axios from './axios'
 const httpURL = '/wypapi'
 export const reqLogin = (data) => Axios(httpURL + '/teachtool/login/loginstate',data,'post');
 
-/**
- * 发布文章
- */
-
- /**
-  * 编辑文章
-  */
-
  /**
   * 获取文章列表
   */
-export const reqArticleList = () =>Axios('http://localhost:5000/api/myblog/articleList.do','','get')
+export const reqArticleList = (data) =>Axios('http://localhost:5000/api/myblog/articleList.do',data,'post')
+
+/**
+ * 发布文章
+ */
+export const releaseArticle = (data) => Axios('http://localhost:5000/api/myblog/releaseArticle.do',data,'post')
