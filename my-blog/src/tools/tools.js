da6203a3ebf455068001132cf13e4e56b81841a4
liangrumeng <liangrumeng2015@163.com>
2020-05-09 18:38:18 +0800

    文章排序

--- my-blog/src/tools/tools.js
+++ my-blog/src/tools/tools.js
@@ -2,7 +2,7 @@
  * 时间戳转换成时间  2020-01-02 00：00：00
  */
 export const changeTimeStamp = (timestamp) => {
-  let time1 = new Date(timestamp)
+  let time1 = new Date(Number(timestamp))
   let year = time1.getFullYear();
   let month = time1.getMonth() + 1;
   let day = time1.getDate();
@@ -13,6 +13,6 @@ export const changeTimeStamp = (timestamp) => {
   day = day < 10 ? ('0' + day) : day
   hour = hour < 10 ? '0' + hour : hour
   min = min < 10 ? '0' + min : min
-  seconds = seconds < 10 ? '-' + seconds : seconds
+  seconds = seconds < 10 ? '0' + seconds : seconds
   return `${year}-${month}-${day} ${hour}:${min}:${seconds}`
 }
